f9ab10d48d013768ac6037e8f2cd4bfecfd4d168
Daniel Gauthier <zambreality@gmail.com>
2017-10-17 07:31:51 -0400

    A few more structural items added.

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <!DOCTYPE html>
+
+<html lang="en">
+    
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
+        <meta name="name" content="Daniel Gauthier">
+        
+        <?php wp_head(); ?>
+        
+    </head>
+    
+    <body>
+        
+        
+        
+    </body>
+    
+</html>
